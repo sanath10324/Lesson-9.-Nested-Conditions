13c2218b74fc8bd3817d022df00daba58fdbb3f6
sanath10324 <sanathkatkamgamer@gmail.com>
2026-01-08 15:34:51 +0400

    Age Checker

---
+++ age_checking.py
@@ -0,0 +1,6 @@
+age = int(input("Please enter your age:"))
+
+if age >= 10 and age <= 20:
+    print("You are allowed to enter the class")
+else:
+    print("You are not allowed to enter the class")
